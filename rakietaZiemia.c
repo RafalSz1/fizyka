9cdb48226d231f67f4f50867d84bd502398800db
RafalSz1 <332718@stud.umk.pl>
2026-05-06 17:06:24 +0200

    Update rakietaZiemia.c

--- rakietaZiemia.c
+++ rakietaZiemia.c
@@ -7,9 +7,10 @@
 #define RZ 6371000.0
 #define RMAX 405696000.0
 #define EPS_R 10.0
-#define EPS_V 0.01
+#define EPS_V 0.1
 
-void symulacja(double alpha_deg, const char* filename) {
+void symulacja(double alpha_deg, const char* filename) 
+{
     FILE *f = fopen(filename, "w");
     double alpha = alpha_deg * M_PI / 180.0;
     double vII = sqrt(2.0 * G * MZ / RZ);
@@ -17,11 +18,12 @@ void symulacja(double alpha_deg, const char* filename) {
     double rx = RZ * cos(alpha), ry = RZ * sin(alpha);
     double vx = vII * cos(alpha), vy = vII * sin(alpha);
     double zx = 0, zy = 0, vzx = 0, vzy = 0;
-    double t = 0, dt = 1.0;
+    double t = 0, dt = 1.0, dt_prev = 1.0;
     
     double Fold_x = 0, Fold_y = 0;
 
-    while (sqrt(rx*rx + ry*ry) < RMAX) {
+    while (sqrt(rx*rx + ry*ry) < RMAX) 
+	{
         double dx = rx - zx;
         double dy = ry - zy;
         double dist = sqrt(dx*dx + dy*dy);
@@ -31,15 +33,28 @@ void symulacja(double alpha_deg, const char* filename) {
         double Fy = -G * MZ * MR * dy / dist3;
 
         double dtr = sqrt((2.0 * MR * EPS_R) / sqrt(Fx*Fx + Fy*Fy));
-        double dtv = 1e6;
+        double dtv = 1000.0;
         
-        if (t > 0) {
-            double dFdt = sqrt(pow(Fx - Fold_x, 2) + pow(Fy - Fold_y, 2)) / dt;
-            if (dFdt > 0) dtv = sqrt((2.0 * MR * EPS_V) / dFdt);
+        if (t > 0) 
+		{
+			double dFx = Fx - Fold_x;
+			double dFy = Fy - Fold_y;
+			    
+			double dF_dt_mag = sqrt(dFx*dFx + dFy*dFy) / dt;
+			    
+			if (dF_dt_mag > 0) 
+			{
+			    dtv = sqrt((2.0 * MR * dt_prev * EPS_V) / dF_dt_mag);
+			}
         }
         
-        dt = (dtr < dtv) ? dtr : dtv;
-        if (dt < 0.001) dt = 0.001;
+        dt_prev = dt;
+        
+        if (dtr < dtv) dt = dtr;
+        else dt = dtv;
+        
+        if (dt > 1000.0) dt = 1000.0;
+        if (dt < 0.01) dt = 0.01;
 
         fprintf(f, "%e %e %e %e %e\n", t, rx, ry, dtr, dtv);
 
@@ -62,7 +77,8 @@ void symulacja(double alpha_deg, const char* filename) {
     printf("Kat %.1f: Czas dolotu = %.2f h\n", alpha_deg, t / 3600.0);
 }
 
-int main() {
+int main() 
+{
     symulacja(0.0, "wyniki_0.txt");
     symulacja(45.0, "wyniki_45.txt");
     return 0;
